570b26438b960b27dbdaed5563e77b485fea4e95
codingepaduli <padulodeveloper@protonmail.com>
2024-02-13 21:04:15 +0100

    Minor Fix - fixed script against shellcheck linter

--- script.sh
+++ script.sh
@@ -6,13 +6,13 @@
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Get the Mozilla profile folder from the file profiles.ini
-firefoxProfileFolder=`crudini --get "$HOME/.mozilla/firefox/profiles.ini" Profile0 Path`
+firefoxProfileFolder=$(crudini --get "$HOME/.mozilla/firefox/profiles.ini" Profile0 Path)
 
 # Mozilla Firefox bookmarks backup folder
 firefoxBackupFolder="$HOME/.mozilla/firefox/$firefoxProfileFolder/bookmarkbackups"
 
 # Set the last backup
-lastUpdatedFile=`ls -t "$firefoxBackupFolder" | head -n1`
+lastUpdatedFile=$(ls -t "$firefoxBackupFolder" | head -n1)
 
 # Set the output folder
 outputFolder="$currentDir/output"
@@ -37,9 +37,10 @@ developingBookmarksFilePath="$HOME/Sviluppo/SVN2/codingepaduli/content/interesti
 educationalBookmarksFilePath="$HOME/Sviluppo/SVN2/codingepaduli/content/interesting/Educational.md"
 
 # print the Markdown header (sed is used to update the publishing date)
-cat "$currentDir/Others.header.md" | sed "s/%/`date --date="yesterday" '+%Y-%m-%d'`/g" > "$othersBookmarksFilePath"
-cat "$currentDir/Developing.header.md" | sed "s/%/`date --date="yesterday" '+%Y-%m-%d'`/g" > "$developingBookmarksFilePath"
-cat "$currentDir/Educational.header.md" | sed "s/%/`date --date="yesterday" '+%Y-%m-%d'`/g" > "$educationalBookmarksFilePath"
+DATE_YESTERDAY=$(date --date="yesterday" '+%Y-%m-%d')
+cat "$currentDir/Others.header.md" | sed "s/%/$DATE_YESTERDAY/g" > "$othersBookmarksFilePath"
+cat "$currentDir/Developing.header.md" | sed "s/%/$DATE_YESTERDAY/g" > "$developingBookmarksFilePath"
+cat "$currentDir/Educational.header.md" | sed "s/%/$DATE_YESTERDAY/g" > "$educationalBookmarksFilePath"
 
 # print the Markdown content
 python3 "$currentDir/exportToMarkdown.py" "$outputFolder/bookmarks.json" --denied 'Media e Download' 'Giustizia e leggi' 'Scuola' 'Concorsi' 'Altri segnalibri sparsi' 'Blog News e Link' '.Net' 'Scienze' 'Altri segnalibri' 'unfiled' 'toolbar' >> "$developingBookmarksFilePath"
